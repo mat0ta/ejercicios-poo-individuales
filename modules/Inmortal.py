4d919fb3187d9ba81feb2970abc44b21918a3ea2
Matota <72810000+mat0ta@users.noreply.github.com>
2022-03-25 12:19:35 +0100

    Update Inmortal.py

---
+++ modules/Inmortal.py
@@ -0,0 +1,13 @@
+class Yin: pass
+class Yang:
+    def __del__(self):
+        print("Yang destruido")
+
+yin = Yin()
+yang = Yang()
+yin.yang = yang
+
+print(yang)
+print(yang is yin.yang)
+del(yang)
+print("?")
